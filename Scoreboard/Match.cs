ef8e4f0f4ed5a6427c09fc78751280e705372575
Kanstantsin Yarash <kanstantsin.yarash@gmail.com>
2023-10-05 17:26:54 +0200

    scoreboard: add logic to Scoreboard; update test to fit requirments;

--- Scoreboard/Match.cs
+++ Scoreboard/Match.cs
@@ -1,13 +1,65 @@
 ﻿namespace Scoreboard
 {
-    public class Match
+    public class Match : IComparable<Match>
     {
-        public TeamScore Home { get; set; }
-        public TeamScore Away { get; set; }
+        public TeamScore Home { get; }
+        public TeamScore Away { get; }
+
+        public DateTimeOffset StartTime { get; private set; }
+        public DateTimeOffset? EndTime { get; private set; }
+
+        public event EventHandler Finished = delegate { };
+        public event EventHandler Updated = delegate { };
+
+        public Match(Team home, Team away)
+        {
+            Home = new TeamScore { Team = home, Score = 0 };
+            Away = new TeamScore { Team = away, Score = 0 };
+        }
+
+        public Match Start()
+        {
+            StartTime = DateTimeOffset.UtcNow;
+            return this;
+        }
 
         public Match UpdateScore(int home, int away)
         {
-            throw new NotImplementedException();
+            Home.Score = home;
+            Away.Score = away;
+
+            Updated.Invoke(this, EventArgs.Empty);
+
+            return this;
+        }
+
+        public Match Finish()
+        {
+            EndTime = DateTimeOffset.UtcNow;
+            Finished.Invoke(this, EventArgs.Empty);
+
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return $"{Home.Team.Name} {Home.Score} - {Away.Team.Name} {Away.Score}";
+        }
+
+        public int CompareTo(Match? other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+
+            var totalScore = Home.Score + Away.Score;
+            var otherTotalScore = other.Home.Score + other.Away.Score;
+
+            if (totalScore > otherTotalScore)
+            {
+                return -1;
+            }
+
+            return StartTime.CompareTo(other.StartTime);
         }
     }
 }
--- Scoreboard/Scoreboard.cs
+++ Scoreboard/Scoreboard.cs
@@ -1,15 +1,40 @@
 ﻿namespace Scoreboard
 {
-    public class Scoreboard
+    public class Scoreboard : IDisposable
     {
+        private readonly List<Match> _matches = new();
+
         public Match Start(Team home, Team away)
         {
-            throw new NotImplementedException();
+            var match = new Match(home, away)
+                .Start();
+
+            match.Finished += MatchOnFinished;
+
+            _matches.Add(match);
+            return match;
         }
 
         public IList<Match> GetSummary()
         {
-            throw new NotImplementedException();
-        }   
+            _matches.Sort();
+            return _matches;
+        }
+
+        public void Dispose()
+        {
+            foreach (var match in _matches)
+            {
+                match.Finished -= MatchOnFinished;
+            }
+        }
+
+        private void MatchOnFinished(object? sender, EventArgs e)
+        {
+            if (sender is Match match)
+            {
+                _matches.Remove(match);
+            }
+        }
     }
 }
